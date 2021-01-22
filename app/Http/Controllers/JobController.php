aa1958fe8194e50c2411e524f653b0df3afeace0
田闯 <1509760688@qq.com>
2021-01-22 10:44:56 +0800

    no message

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -16,8 +16,8 @@ class JobController extends Controller
 
 
         $gethrpc=new Eth(config('app.eth'));//测试网络
-        $result=$gethrpc->personal_newAccount('fyvIRYwxpfT$13KA');
-        //$result = $gethrpc->personal_unlockAccount(config("app.getFeeAddress"),config("app.getFeeAddressPassword"));//解锁
+        //$result=$gethrpc->personal_newAccount('fyvIRYwxpfT$13KA');
+        $result = $gethrpc->personal_unlockAccount(config("app.getFeeAddress"),config("app.getFeeAddressPassword"));//解锁
 
         dd($result);
 //        $infura=new Eth('https://mainnet.infura.io/v3/ca6382c272c94b5ab65937ce7213e94f');//infura网络
--- config/app.php
+++ config/app.php
@@ -128,7 +128,7 @@ return [
     'ethAddress' => '0x095A61BBc66af5D53408A95c33dd4AabD4416b94',
 //    'erc20Address' => '0xa25b48DE6ae47e59cBd9f1Ba0f853dC110E5C616',
 //    'ethAddress' => '0xa25b48DE6ae47e59cBd9f1Ba0f853dC110E5C616',
-    'getFeeAddress' => '0xcc783a8c3ec730e713e4a6214405c2211e34ebcc',
+    'getFeeAddress' => '0xee724fa614121e2f43021b05d679b932c1e1c385',
     'getFeeAddressPassword' => 'fyvIRYwxpfT$13KA',
     'AddressPassword' => 'vd!LiedNJ9DkGRpA',
 
